66fc939de8f7c32c1651da324591bc747c0cd399
akburden <56166983+akburden@users.noreply.github.com>
2020-08-27 17:14:06 +0200

    Add files via upload
    personDTO changed to personDTO

--- Mapper.java
+++ Mapper.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class PersonMapper {
 
-    public static List<personDTO> map(HashMap<String, ArrayList<String>> data){
-        ArrayList<personDTO> peopleDTO = new ArrayList<personDTO>();
+    public static List<PersonDTO> map(Map<String, List<String>> data){
+        ArrayList<PersonDTO> peopleDTO = new ArrayList<PersonDTO>();
         for(int i = 0; data.get("NAME").size(); i++){
-            peopleDTO.add(new personDTO(data.get("NAME").get(i), Integer.parseInt(data.get("AGE").get(i))));
+            peopleDTO.add(new PersonDTO(data.get("NAME").get(i), Integer.parseInt(data.get("AGE").get(i))));
         }
         return peopleDTO;
     }
---
+++ PersonDTO.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class PersonDTO {
+    private String name;
+    private int age;
+
+}
+
